b6aeb266683e1d4143f0df9938df4207574d174e
Gary Carino <garycarino@gmail.fake>
2013-05-08 00:40:46 +1000

    Initialized jsquiz files.

---
+++ jsquiz/jsquiz.js
@@ -0,0 +1,10 @@
+var allQuestions = [
+	{
+		question: "Which American State is the best?", 
+		choices: ['New York', 'Washington', 'Texas', 'California','Nevada'],
+		correctAnswer: 3
+	},
+	{
+		
+	}
+];
