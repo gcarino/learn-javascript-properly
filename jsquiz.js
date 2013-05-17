19699315e778bd4005d2f30f2e8791960f66ee7c
Gary Carino <garycarino@gmail.fake>
2013-05-17 10:18:48 +1000

    Made some minor code tweaks and variable names. Reorganized code pushing the functions to the bottom. No real functionality changes.

--- jsquiz.js
+++ jsquiz.js
@@ -1,59 +1,3 @@
-function createQuestionElement(index) {
-    var qDiv = document.createElement('div');
-    qDiv.setAttribute('id', 'question');
-
-    var header = document.createElement('h2');
-    header.innerHTML = 'Question ' + (index + 1) + ":";
-
-    var question = document.createElement('p');
-    question.innerHTML = questions[index].question;
-
-    qDiv.appendChild(header);
-    qDiv.appendChild(question);
-    qDiv.appendChild(createRadios(index));
-    
-    return qDiv;
-}
-
-function createRadios(index) {
-    var radioList = document.createElement('ul');
-    var item = '';
-    var input = '';
-    for (var i = 0; i < questions[index].choices.length; i++) {
-        item = document.createElement('li');
-        input = '<input type="radio" name="answer" value=' + i + ' />';
-        input += questions[index].choices[i];
-
-        item.innerHTML = input;
-        radioList.appendChild(item);
-    }
-
-    return radioList;
-}
-
-function displayScore() {
-    var para = document.createElement('p');
-    var numCorrect = 0;
-
-    for (var i = 0; i < selections.length; i++) {
-        if (selections[i] == questions[i].correctAnswer) {
-            numCorrect++;
-        }
-    }
-
-    para.innerHTML = 'You got ' + numCorrect + ' questions out of ' +
-        questions.length + ' right!!!';
-    return para;
-}
-
-function select() {
-    for (var i = 0; i < radios.length; i++) {
-        if (radios[i].checked) {
-            selections[questionCounter] = radios[i].value;
-        }
-    }
-}
-
 var questionCounter = 0;
 var selections = [];
 var radios = document.getElementsByName('answer');
@@ -92,7 +36,7 @@ $('#prev').on('click', function () {
     questionCounter--;
     $('#question').remove();
 
-    if(questionCounter == 0){
+    if(questionCounter === 0){
         document.getElementById('prev').setAttribute('style', 'display:none');
     }
 
@@ -102,3 +46,61 @@ $('#prev').on('click', function () {
 });
 
 
+function createQuestionElement(index) {
+    var qElement = document.createElement('div');
+    qElement.setAttribute('id', 'question');
+
+    var header = document.createElement('h2');
+    header.innerHTML = 'Question ' + (index + 1) + ":";
+    qElement.appendChild(header);
+
+    var question = document.createElement('p');
+    question.innerHTML = questions[index].question;
+    qElement.appendChild(question);
+
+    var radioButtons = createRadios(index);
+    qElement.appendChild(radioButtons);
+    
+    return qElement;
+}
+
+function createRadios(index) {
+    var radioList = document.createElement('ul');
+    var item = '';
+    var input = '';
+    for (var i = 0; i < questions[index].choices.length; i++) {
+        item = document.createElement('li');
+        input = '<input type="radio" name="answer" value=' + i + ' />';
+        input += questions[index].choices[i];
+
+        item.innerHTML = input;
+        radioList.appendChild(item);
+    }
+
+    return radioList;
+}
+
+function displayScore() {
+    var score = document.createElement('p');
+    var numCorrect = 0;
+
+    for (var i = 0; i < selections.length; i++) {
+        if (selections[i] === questions[i].correctAnswer) {
+            numCorrect++;
+        }
+    }
+    console.log(typeof(selections[0]));
+    console.log(typeof(questions[0].correctAnswer));
+
+    score.innerHTML = 'You got ' + numCorrect + ' questions out of ' +
+        questions.length + ' right!!!';
+    return score;
+}
+
+function select() {
+    for (var inputVal = 0; inputVal < radios.length; inputVal++) {
+        if (radios[inputVal].checked) {
+            selections[questionCounter] = radios[inputVal].value;
+        }
+    }
+}
